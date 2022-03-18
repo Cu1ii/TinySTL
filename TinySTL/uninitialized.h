3b8fd4fe8f4bbd493f71a06426c9002ab261f91a
Cu1 <cu1universe@163.com>
2022-03-18 13:20:53 +0800

    finish uninitialized.h

--- TinySTL/uninitialized.h
+++ TinySTL/uninitialized.h
@@ -144,6 +144,119 @@ namespace tstl
                                     value_type>{});
     }
 
-} // namespace tstl
+    /**
+     * uninitialized_fill_n
+     * 从 first 开始填充 n 个元素, 返回填充结束的位置
+     */
+    template <class ForwardIter, class Size, class T>
+    ForwardIter
+    unchecked_uninit_fill_n(ForwardIter first, Size n, const T& value, std::true_type)
+    {
+        return mystl::fill_n(first, n, value);
+    }
 
+    template <class ForwardIter, class Size, class T>
+    ForwardIter
+    unchecked_uninit_fill_n(ForwardIter first, Size n, const T& value, std::false_type)
+    {
+        auto cur = first;
+        try
+        {
+            for (; n > 0; --n, ++cur)
+            {
+                mystl::construct(&*cur, value);
+            }
+        }
+        catch (...)
+        {
+            for (; first != cur; ++first)
+                mystl::destroy(&*first);
+        }
+        return cur;
+    }
+
+    template <class ForwardIter, class Size, class T>
+    ForwardIter uninitialized_fill_n(ForwardIter first, Size n, const T& value)
+    {
+        return mystl::unchecked_uninit_fill_n(first, n, value,
+                                              std::is_trivially_copy_assignable<
+                                                      typename iterator_traits<ForwardIter>::
+                                                      value_type>{});
+    }
+
+    /**
+     * uninitialized_move
+     * 把 [firts, last) 上的内容移动到以 result 为起始处的空间, 返回移动结束的位置
+     */
+    template <class InputIter, class ForwardIter>
+    ForwardIter
+    unchecked_uninit_move(InputIter first, InputIter last, ForwardIter result, std::true_type)
+    {
+        return mystl::move(first, last, result);
+    }
+
+    template <class InputIter, class ForwardIter>
+    ForwardIter
+    unchecked_uninit_move(InputIter first, InputIter last, ForwardIter result, std::false_type)
+    {
+        ForwardIter cur = result;
+        try
+        {
+            for (; first != last; ++first, ++cur)
+            {
+                mystl::construct(&*cur, mystl::move(*first));
+            }
+        }
+        catch (...)
+        {
+            mystl::destroy(result, cur);
+        }
+        return cur;
+    }
+
+    template <class InputIter, class ForwardIter>
+    ForwardIter uninitialized_move(InputIter first, InputIter last, ForwardIter result)
+    {
+        return mystl::unchecked_uninit_move(first, last, result,
+                                            std::is_trivially_move_assignable<
+                                                    typename iterator_traits<InputIter>::
+                                                    value_type>{});
+    }
+
+    /**
+     * uninitialized_move_n
+     * 把 [first, first + n) 上的内容移动到以 result 为起始处的空间, 返回移动结束的位置
+     */
+     template <class InputIter, class Size, class ForwardIter>
+     ForwardIter
+     unchecked_uninit_move_n(InputIter first, Size n, ForwardIter result, std::true_type)
+    {
+         return tstl::move(first, first + n, result);
+    }
+
+    template <class InputIter, class Size, class ForwardIter>
+    ForwardIter
+    unchecked_uninit_move_n(InputIter first, Size n, ForwardIter result, std::false_type)
+    {
+        auto cur = result;
+        try
+        {
+            for (; n > 0; --n ,++cur, ++first)
+            {
+                tstl::construct(&*cur, tstl::move(*first));
+            }
+        }
+    }
+
+    template <class InputIter, class Size, class ForwardIter>
+    ForwardIter
+    uninitialized_move_n(InputIter first, Size n, ForwardIter result)
+    {
+        return tstl::unchecked_uninit_move_n(first, n, result,
+                                             std::is_trivially_copy_assignable<
+                                             typename iterator_traits<InputIter>
+                                             ::value_type>{});
+    }
+
+} // namespace tstl
 #endif //TINYSTL_UNINITIALIZED_H
