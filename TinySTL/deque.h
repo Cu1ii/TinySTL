d3bf143e47a4b89f1eca07f56e282a8c652fb1a8
Cu1 <cu1universe@163.com>
2022-05-18 21:35:57 +0800

    update deque.h insert, emplace_*

--- TinySTL/deque.h
+++ TinySTL/deque.h
@@ -278,11 +278,11 @@ namespace mystl
             if (&rhs != this)
             {
                 if (len >= rhs.size())
-                    erase(copy(rhs.begin(), rhs.end(), start), finish);
+                    erase(mystl::copy(rhs.begin(), rhs.end(), start), finish);
                 else
                 {
                     const_iterator mid = rhs.begin() + distance_type(len);
-                    copy(rhs.begin(), mid, start);
+                    mystl::copy(rhs.begin(), mid, start);
                     insert(finish. mid, rhs.end());
                 }
             }
@@ -291,6 +291,13 @@ namespace mystl
 
         deque& operator=(deque&& rhs)
         {
+            clear();
+            start = mystl::move(rhs.start);
+            finish = mystl::move(rhs.finish);
+            map_ = rhs.map_;
+            map_size = rhs.map_size;
+            rhs.map_ = nullptr;
+            rhs.map_size = 0;
             return *this;
         }
 
@@ -301,7 +308,7 @@ namespace mystl
             return *this;
         }
 
-        void swap(deque& other)
+        void swap(deque& other) noexcept
         {
             mystl::swap(start, other.start);
             mystl::swap(finish, other.finish);
@@ -434,6 +441,7 @@ namespace mystl
             copy_assign(ilist.begin(), ilist.end(), mystl::forward_iterator_tag());
         }
 
+        // TODO
         // emplace_front() / emplace_back() / emplace()
         template <class ...Args>
         void emplace_front(Args&& ...args);
@@ -453,18 +461,34 @@ namespace mystl
                 --start.cur;
             }
             else
-                push_front_aux(value);
+            {
+                reserve_elements_at_front(1);
+                try
+                {
+                    --start;
+                    data_allocator::construct(start.cur, value);
+                }
+                catch (...)
+                {
+                    ++start;
+                    throw ;
+                }
+            }
         }
 
         void push_back(const value_type& value)
         {
             if (finish.cur != finish.last - 1)
             {
-                mystl::construct(finish.cur, value);
+                data_allocator::construct(finish.cur, value);
                 ++finish.cur;
             }
             else
-                push_back_aux(value);
+            {
+                reserve_elements_at_back(1);
+                data_allocator::construct(finish.cur);
+                ++finish;
+            }
         }
 
         void push_front(value_type&& value) { emplace_front(mystl::move(value)); }
@@ -477,25 +501,331 @@ namespace mystl
             if (finish.cur != finish.first)
             {
                 --finish.cur;
-                mystl::destroy(finish.cur);
+                data_allocator::destroy(finish.cur);
             }
             else
-                pop_back_aux();
+            {
+                deallocate_node(finish.first);
+                finish.set_node(finish.node - 1);
+                finish.cur = finish.last - 1;
+                data_allocator::destroy(finish.cur);
+            }
         }
 
         void pop_front()
         {
             if (start.cur != start.last - 1)
             {
-                mystl::destroy(start.cur);
+                data_allocator::destroy(start.cur);
                 ++start.cur;
             }
             else
-                pop_front_aux();
+            {
+                data_allocator::destroy(start.cur);
+                deallocate_node(start.first);
+                start.set_node(start.node + 1);
+                start.cur = start.first;
+            }
         }
 
         // insert()
+
+        iterator insert(iterator postiton, const value_type& value)
+        {
+            if (postiton.cur == start.cur)
+            {
+                push_front(value);
+                return start;
+            }
+            else if (postiton.cur == finish.cur)
+            {
+                push_back(value);
+                iterator tmp = finish;
+                --tmp;
+                return tmp;
+            }
+            else
+                return insert_aux(postiton, value);
+        }
+
+        iterator insert(iterator position)
+        {
+            return insert(position, value_type());
+        }
+
+        iterator insert(iterator position, value_type&& value)
+        {
+            emplace(position, mystl::move(value));
+        }
+
+        void insert(iterator position, size_type n, const value_type& value);
+
+        template <class Iter, typename std::enable_if<
+                mystl::is_input_iterator<Iter>::value, int>::type = 0>
+        void insert(iterator position, Iter first, Iter last)
+        {
+            insert(pos, first, last, iterator_category(first));
+        }
+
+        void resize(size_type new_size)
+        {
+            resize(new_size, value_type());
+        }
+
+
+    public:
+        // erase() / clear()
+
+        iterator erase(iterator position)
+        {
+            iterator nex = pos;
+            ++nex;
+            auto index = pos - start;
+            if (index < (size() >> 1))
+            {
+                mystl::copy_backward(start, position, nex);
+                pop_front();
+            }
+            else
+            {
+                mystl::copy(nex, finish, pos);
+                pop_back();
+            }
+            return start + index;
+        }
+
+        iterator erase(iterator first, iterator last);
+
+        void clear();
+
+    protected:
+        // Internal construction/destruction
+        void create_map_and_nodes(size_type num_elements);
+
+        void destroy_map_and_nodes();
+
+        void fill_initialize(size_type n, const value_type& value);
+
+        template <class InputIter>
+        void range_initialize(InputIter first, InputIter last, input_iterator_tag);
+
+        template <class ForwardIter>
+        void range_initialize(ForwardIter first, ForwardIter last, forward_iterator_tag);
+
+
+    protected:
+        // Internal assign functions
+        void fill_assign(size_type n, const value_type& value);
+
+        template <class InputIter>
+        void copy_assign(InputIter first, InputIter last, input_iterator_tag);
+
+        template <class ForwardIter>
+        void copy_assign(ForwardIter first, ForwardIter last, forward_iterator_tag);
+
+    protected:
+        // Internal insert functions
+        template <class InputIter>
+        void insert(iterator position, InputIter first, InputIter last, input_iterator_tag);
+
+        template <class ForwardIter>
+        void insert(iterator position, ForwardIter first, ForwardIter last, forward_iterator_tag);
+
+        template <class ...Args>
+        iterator insert_aux(iterator position, Args&& ...args);
+
+        iterator insert_aux(iterator position, const value_type& value);
+
+        void insert_aux(iterator position, size_type n, const value_type& value);
+
+        template <class ForwardIter>
+        void insert_aux(iterator position, ForwardIter first, ForwardIter last, size_type n);
+
+        iterator reserve_elements_at_front(size_type n)
+        {
+            size_type vacancies = start.cur - start.first;
+            if (n > vacancies)
+                new_elements_at_front(n - vacancies);
+            return start - static_cast<difference_type>(n);
+        }
+
+        iterator reserve_elements_at_back(size_type n)
+        {
+            size_type vacancies = (finish.last - finish.cur) - 1;
+            if (n > vacancies)
+                new_elements_at_back(n - vacancies);
+            return finish + static_cast<difference_type>(n);
+        }
+
+        void new_elements_at_front(size_type new_elements);
+
+        void new_elements_at_back(size_type new_elements);
+
+        void desrtoy_nodes_at_front(iterator before_start);
+
+        void desrtoy_nodes_at_back(iterator after_finish);
+
+    protected:
+        // Allocation of map and nodes
+        // 给 map 进行扩容以保证可以容纳新的节点. 实际上并没有增加节点
+        // 同时扩容操作会改变 map_pointer 从而使得之前的 map_pointer 以及 deque 迭代器失效
+        void resever_map_at_back(size_type nodes_to_add = 1)
+        {
+            if (nodes_to_add + 1 > map_size - (finish.node - map_))
+                reallocate_map(nodes_to_add, false);
+        }
+
+        void resever_map_at_front(size_type nodes_to_add = 1)
+        {
+            if (nodes_to_add + 1 > start.node - map_)
+                reallocate_map(nodes_to_add, true);
+        }
+
+        void reallocate_map(size_type nodes_to_add, bool add_at_front);
+
+        pointer allocate_node() { return data_allocator::allocate(buffer_size); }
+
+        void deallocate_node(pointer x) { data_allocator::deallocate(x, buffer_size); }
     };
+
+    template <class T>
+    template <class ...Args>
+    void deque<T>::emplace_front(Args&& args...)
+    {
+        if (start.cur != start.first)
+        {
+            data_allocator::construct(start.cur - 1, mystl::forward<Args>(args)...);
+            --start.cur;
+        }
+        else
+        {
+            reserve_elements_at_front(1);
+            try
+            {
+                --start;
+                data_allocator::construct(start.cur, mystl::forward<Args>(args)...);
+            }
+            catch (...)
+            {
+                ++start;
+                throw ;
+            }
+        }
+    }
+
+    template <class T>
+    template <class ...Args>
+    void deque<T>::emplace_back(Args&& args...)
+    {
+        if (finish.cur != finish.last - 1)
+        {
+            data_allocator::construct(finish.cur, mystl::forward<Args>(args)...);
+            ++finish.cur;
+        }
+        else
+        {
+            reserve_elements_at_back(1);
+            data_allocator::construct(finish.cur, mystl::forward<Args>(args)...);
+            ++finish;
+        }
+    }
+
+    template <class T>
+    template <class ...Args>
+    typename deque<T>::iterator
+    deque<T>::emplace(iterator pos, Args&& args...)
+    {
+        if (pos.cur == start.cur)
+        {
+            emplace_front(mystl::forward<Args>(args)...);
+            return start;
+        }
+        else if (pos.cur == finish.cur)
+        {
+            emplace_back(mystl::forward<Args>(args)...);
+            return finish - 1;
+        }
+        return insert_aux(pos, mystl::forward<Args>(args)...);
+    }
+
+    template <class T>
+    void deque<T>::insert(iterator position, size_type n, const value_type& value)
+    {
+        if (position.cur == start.cur)
+        {
+            iterator new_start = reserve_elements_at_front(n);
+            mystl::uninitialized_fill(new_start, start, value);
+            start = new_start;
+        }
+        else if (position.cur == finish.cur)
+        {
+            iterator new_finish = reserve_elements_at_back(n);
+            mystl::uninitialized_fill(finish, new_finish, value);
+            finish = new_finish;
+        }
+        else
+            insert_aux(position, n, value);
+    }
+
+    template <class T>
+    template <class InputIter>
+    void deque<T>::insert(iterator position, InputIter first, InputIter last, input_iterator_tag)
+    {
+        if (last <= first) return ;
+        const size_type n = mystl::distance(first, last);
+        const size_type elems_before = position - start;
+        if (elems_before < (size() / 2))
+            reserve_elements_at_front(n);
+        else
+            reserve_elements_at_back(n);
+        position = start + elems_before;
+        auto cur = --last;
+        for (size_type i = 0; i < n; ++i, --cur)
+            insert(position, *cur);
+    }
+
+    template <class T>
+    template <class ForwardIter>
+    void deque<T>::insert(iterator position, ForwardIter first, ForwardIter last, forward_iterator_tag)
+    {
+        if (last <= first) return ;
+        const size_type n = mystl::distance(first, last);
+        if (position.cur == start.cur)
+        {
+            reserve_elements_at_front(n);
+            auto new_start = start - n;
+            try
+            {
+                mystl::uninitialized_copy(first, last, new_start);
+                start = new_start;
+            }
+            catch (...)
+            {
+                desrtoy_nodes_at_front(new_start);
+                throw ;
+            }
+        }
+        else if (position.cur == finish.cur)
+        {
+            reserve_elements_at_back(n);
+            auto new_finish = finish + n;
+            try
+            {
+                mystl::uninitialized_copy(first, last, finish);
+                finish = new_finish;
+            }
+            catch (...)
+            {
+                desrtoy_nodes_at_back(new_finish);
+                throw ;
+            }
+        }
+        else
+            insert_aux(position, first, last, n);
+    }
+
+    template <class T>
+
 
 }
 
